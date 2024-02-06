4e97079cce7be6dd4f7255dc3ddd8a3919038187
dmarrs-qelZcl <dmarrs@instructors.2u.com>
2024-02-05 18:46:54 -0800

    added user creation with hashing

--- server/prisma/migrations/20240206024448_schemas/migration.sql
+++ server/prisma/migrations/20240206024448_schemas/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "username" VARCHAR(30) NOT NULL,
     "email" VARCHAR(50) NOT NULL,
-    "password" VARCHAR(20) NOT NULL,
+    "password" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,13 +1,26 @@
 const router = require('express').Router();
+const { hashSync } = require('bcrypt')
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
 router.get('/', async (req, res) => {
-    const users = await prisma.user.findMany({
-
-    })
+    const users = await prisma.user.findMany({})
     res.json(users)
+})
+
+router.post('/', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const user = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashSync(password, 10)
+        }
+    })
+
+    res.json(user)
 })
 
 module.exports = router;
