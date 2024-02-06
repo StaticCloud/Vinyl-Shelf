4b108963f37db5d6e99c675ea6c444911b0400eb
dmarrs-qelZcl <dmarrs@instructors.2u.com>
2024-02-05 19:18:40 -0800

    implemented auth middleware in backend

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { hashSync } = require('bcrypt')
+const { hashSync, compareSync } = require('bcrypt')
 const { PrismaClient } = require('@prisma/client');
+const { signToken } = require('../../utils/auth')
 
 const prisma = new PrismaClient();
 
@@ -24,6 +25,33 @@ router.post('/', async (req, res) => {
         res.json(user)
     } catch (error) {
         res.status(400).json({ message: 'Something went wrong!' })
+    }
+})
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        })
+
+        if (!user) {
+            throw new Error('Could not find a user with that email!')
+        }
+
+        const pwCheck = await compareSync(password, user.password)
+
+        if (!pwCheck) {
+            throw new Error('Invalid password!')
+        }
+
+        const token = signToken(user)
+        res.json({ token, user })
+    } catch (error) {
+        res.status(400).json({ message: "Something went wrong!" })
     }
 })
 
---
+++ server/utils/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.SECRET;
+
+module.exports = {
+    authMiddleware: (req, res, next) => {
+        let token = req.query.token || req.headers.authorization;
+
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
+
+        if (!token) {
+            return res.status(400).json({ message: 'You have no token!' });
+        }
+
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+            return res.status(400).json({ message: 'Invalid token!' });
+        }
+
+        next();
+    },
+    signToken: ({ id, email, username }) => {
+        return jwt.sign({
+            data: {
+                id,
+                email,
+                username
+            }
+        }, secret, { expiresIn: '2h' })
+    }
+}
