ef194d04da09fe6c87ecca219b889d52cfc741a8
dmarrs-qelZcl <dmarrs@instructors.2u.com>
2024-02-08 13:33:01 -0800

    added nav links for authenticated users

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom"
 import home_light from '../assets/home_light.svg'
 import search_light from '../assets/search_light.svg'
 import login_light from '../assets/login_light.svg'
+import profile_light from '../assets/profile_light.svg'
+import logout_light from '../assets/logout_light.svg'
+import Auth from '../utils/auth';
 
 const NavItem = styled.div`
     height: 60px;
@@ -35,14 +38,30 @@ export const Nav = () => {
             </Link>
             <Link to="/search">
                 <NavItem icon={search_light}>
-                    
+
                 </NavItem>
             </Link>
-            <Link to="/login">
-                <NavItem icon={login_light}>
-                    
-                </NavItem>
-            </Link>
-        </StyledNav>
+            {Auth.loggedIn() ? (
+                <>
+                    <Link to="/profile">
+                        <NavItem icon={profile_light}>
+
+                        </NavItem>
+                    </Link>
+                    <Link onClick={Auth.logout}>
+                        <NavItem icon={logout_light}>
+
+                        </NavItem>
+                    </Link>
+                </>
+            ) : (
+                <Link to="/login">
+                    <NavItem icon={login_light}>
+
+                    </NavItem>
+                </Link >
+            )}
+
+        </StyledNav >
     );
 }
