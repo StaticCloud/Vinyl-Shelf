e1d51025a92aeacf8d8a21f1fa9a33288f063402
dmarrs-qelZcl <dmarrs@instructors.2u.com>
2024-02-08 13:27:48 -0800

    added nav pages for search and login

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,4 +1,15 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom"
+import home_light from '../assets/home_light.svg'
+import search_light from '../assets/search_light.svg'
+import login_light from '../assets/login_light.svg'
+
+const NavItem = styled.div`
+    height: 60px;
+    width: 60px;
+    background-size: cover;
+    background-image: url(${props => props.icon});
+`;
 
 const StyledNav = styled.nav`
     position: fixed;
@@ -6,12 +17,32 @@ const StyledNav = styled.nav`
     width: 100svw;
     height: 60px;
     bottom: 0;
-    justify-content: center;
+    justify-content: space-evenly;
+
+    a {
+        width: 60px;
+        display: block;
+    }
 `;
 
 export const Nav = () => {
     return (
         <StyledNav>
+            <Link to="/">
+                <NavItem icon={home_light}>
+
+                </NavItem>
+            </Link>
+            <Link to="/search">
+                <NavItem icon={search_light}>
+                    
+                </NavItem>
+            </Link>
+            <Link to="/login">
+                <NavItem icon={login_light}>
+                    
+                </NavItem>
+            </Link>
         </StyledNav>
     );
 }
