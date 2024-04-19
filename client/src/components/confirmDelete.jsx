ead22baa788fd6cc93026f11c94acc4444ffe695
dmarrs-qelZcl <dmarrs@instructors.2u.com>
2024-04-19 00:35:13 -0700

    completed styling for shelf deletion popup

--- client/src/components/confirmDelete.jsx
+++ client/src/components/confirmDelete.jsx
@@ -16,7 +16,7 @@ const Backdrop = styled.div`
 
 const ConfirmDeleteWrapper = styled.div`
     width: 600px;
-    height: 30svh;
+    height: auto;
     border-radius: 1rem;
     background-color: ${props => props.theme.secondary};
     overflow-y: scroll;
@@ -33,6 +33,7 @@ const Header = styled.div`
     width: 600px;
 
     & > h1 {
+        font-size: 1rem;
         margin-left: 1rem;
     }
 `;
@@ -55,8 +56,32 @@ const Close = styled.div`
 
 const AlertBody = styled.div`
     margin: 60px 20px 0 20px;
+
+    p {
+        font-size: 1.2rem;
+        margin-bottom: 1rem;
+    }
 `;
 
+const AlertButtonWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 1rem;
+`;
+
+const AlertButton = styled.div`
+    &:hover {
+        cursor: pointer;
+    }
+    
+    flex: 1;
+    text-align: center;
+    padding: 1rem;
+    margin: 2px;
+    border-radius: 1rem;
+
+    background-color: ${props => props.theme.primary};
+`;
 
 export const ConfirmDelete = ({ setShowConfirmDelete, shelfData }) => {
 
@@ -75,7 +100,15 @@ export const ConfirmDelete = ({ setShowConfirmDelete, shelfData }) => {
                     <Close onClick={() => setShowConfirmDelete(false)} />
                 </Header>
                 <AlertBody>
-                    <p>Are you sure you want to delete {shelfData.name}?</p>
+                    <p>Are you sure you want to delete <b>{shelfData.name}</b>?</p>
+                    <AlertButtonWrapper>
+                        <AlertButton>
+                            Delete
+                        </AlertButton>
+                        <AlertButton onClick={() => setShowConfirmDelete(false)}>
+                            Cancel
+                        </AlertButton>
+                    </AlertButtonWrapper>
                 </AlertBody>
             </ConfirmDeleteWrapper>
         </Backdrop>
