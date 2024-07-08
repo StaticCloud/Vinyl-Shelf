0635eed86709b0e05db9586a2ec2a261bd5b1389
dmarrs-qelZcl <dmarrs@instructors.2u.com>
2024-07-07 21:25:39 -0700

    modified server to host dist build generated by vite on the front-end

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+}
+
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}! 🚀🚀🚀`))
